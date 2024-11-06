ac0c2cb2fcc338a5a5304f324ce44988e64f6d83
MaciejKajdaniak <kajdaniak.mk@gmail.com>
2024-11-06 15:18:24 +0100

    blog3

--- NodeBlog/app.js
+++ NodeBlog/app.js
@@ -9,6 +9,7 @@ app.use(express.json());
 // Endpoint do migracji (tworzenie tabel w bazie)
 app.post('/migrate', async (req, res) => {
     try {
+        prisma.migrate;
         res.json({ message: 'Migracja zakończona. Tabele zostały utworzone.' });
     } catch (error) {
         console.error(error);
